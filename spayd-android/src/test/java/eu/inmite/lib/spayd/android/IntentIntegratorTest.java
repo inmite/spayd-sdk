e0fdbd4ad4e53c8c61c90a93c93cd1d08ed06954
Tomas Vondracek <tomas.vondracek@gmail.com>
2014-07-08 21:29:49 +0200

    maven module

--- spayd-android/src/test/java/eu/inmite/lib/spayd/android/IntentIntegratorTest.java
+++ spayd-android/src/test/java/eu/inmite/lib/spayd/android/IntentIntegratorTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class IntentIntegratorTest {
 
 	@Test
